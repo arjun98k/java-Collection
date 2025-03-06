a1a7b99bde43294a085217aa26c60d248ccd374e
arjun98 <arjunkandekar14@gmail.com>
2025-03-06 13:48:21 +0000

    collection

---
+++ arrlist/arrayslist1.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class arrayslist1 {
+
+    public static void main(String[] args) {
+       ArrayList<Integer> l1 = new ArrayList<>();
+       for (int i = 0; i < 7; i++) {
+            l1.add(i);
+       }
+       System.out.println(l1);
+    }
+}
