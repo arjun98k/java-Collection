b3aa16f8347a875af484b9ac45489dcecee2e682
arjun98 <arjunkandekar14@gmail.com>
2025-03-07 10:17:23 +0530

    collection

--- arrlist/arrayslist1.java
+++ arrlist/arrayslist1.java
@@ -4,9 +4,14 @@ public class arrayslist1 {
 
     public static void main(String[] args) {
        ArrayList<Integer> l1 = new ArrayList<>();
-       for (int i = 0; i < 7; i++) {
-            l1.add(i);
-       }
-       System.out.println(l1);
+       l1.add(11);
+       l1.add(12);
+       l1.add(13);
+       l1.add(14);
+       l1.add(15);
+    for (int x  : l1) {
+        System.out.println(x);
+    }
+      
     }
 }
